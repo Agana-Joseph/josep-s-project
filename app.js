844fd8596e4d40de1d5f2d9d1e64d04f1261fc69
Agana-Joseph <josephagana8@gmail.com>
2023-10-18 03:40:37 +0100

    Vinzstel Hospital

---
+++ app.js
@@ -0,0 +1,54 @@
+$('menuBtn').click (function(){
+    $('nav .navigation ul').addClass('active')
+});
+
+$('menuClose').click (function(){
+    $('nav .navigation ul').removeClass('active')
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $('#menuBtn').click(function() {
+//     $( 'nav .navigation ul').addClass('active')
+// });
+
+// $('#menuClose').click(function() {
+//     $( 'nav .navigation ul').removeClass('active')
+// });
